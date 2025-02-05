427a932565d5738519b58f073a85d3215cdc64f0
Lilibeth Neal <t-lneal@microsoft.com>
2025-02-05 17:57:47 -0600

    Added code to GameController

--- GameController.cs
+++ GameController.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading;
 
 namespace Battleship_Group10
 {
     public class GameController
     {
+        Player player1 = new Player("Human");
+        Player player2 = new Player("Computer");
+
+        Grid grid = new Grid();
+
         public GameController()
         {
 
